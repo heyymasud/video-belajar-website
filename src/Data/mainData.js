5ec93bd0b3d896d102da95edc5e2764afa6b5fe4
heyymasud <heyymasud@gmail.com>
2024-07-16 10:22:00 +0700

    feat: header and mainheader

--- src/Data/mainData.js
+++ src/Data/mainData.js
@@ -1,15 +1,23 @@
-const main = [
-    {
-      heroHeader: [
-        {
-          title: "Revolusi Pembelajaran: Temukan Ilmu Baru melalui Platform Video Interaktif!",
-          subtitle: "Temukan ilmu baru yang menarik dan mendalam melalui koleksi video pembelajaran berkualitas tinggi. Tidak hanya itu, Anda juga dapat berpartisipasi dalam latihan interaktif yang akan meningkatkan pemahaman Anda.",
-          button: "Temukan Video Course untuk Dipelajari!",
-          image: './img/hero-header.jpg',
-        },
-      ],
-    },
-  ];
-  
-  export default main;
-  
+const main = {
+  heroHeader: {
+    title:
+      "Revolusi Pembelajaran: Temukan Ilmu Baru melalui Platform Video Interaktif!",
+    subtitle:
+      "Temukan ilmu baru yang menarik dan mendalam melalui koleksi video pembelajaran berkualitas tinggi. Tidak hanya itu, Anda juga dapat berpartisipasi dalam latihan interaktif yang akan meningkatkan pemahaman Anda.",
+    button: "Temukan Video Course untuk Dipelajari!",
+    image: "./img/hero-header.jpg",
+  },
+  mainHeader: {
+    title: "Koleksi Video Pembelajaran Unggulan",
+    subtitle: "Jelajahi Dunia Pengetahuan Melalui Pilihan Kami!",
+    navbar: [
+      "Semua Koleksi",
+      "Pemasaran",
+      "Desain",
+      "Pengembangan Diri",
+      "Bisinis",
+    ],
+  },
+};
+
+export default main;
--- src/Pages/Beranda/index.jsx
+++ src/Pages/Beranda/index.jsx
@@ -1,24 +1,63 @@
-import Button from "../../Components/Elements/Button"
-import Navbar from "../../Components/Layouts/Navbar"
-import main from "../../Data/mainData"
+import { useState } from "react";
+import Button from "../../Components/Elements/Button";
+import Navbar from "../../Components/Layouts/Navbar";
+import main from "../../Data/mainData";
 
 const Beranda = () => {
-    const heroHeader = main[0].heroHeader[0]
-    {console.log(heroHeader.image)}
-    return (
-        <>
-        <Navbar />
-        <main className="h-screen" >
-            <header className="flex flex-col items-center h-full">
-                <section className={`flex flex-col items-center justify-center bg-[url('${heroHeader.image}')] bg-cover bg-center bg-no-repeat h-[400px] w-10/12 px-[140px] py-16 `} >
-                    <h1 className="text-3xl xl:text-5xl font-semibold text-slate-900 text-center font-pop">{heroHeader.title}</h1>
-                    <p className="text-slate-500 text-center">{heroHeader.subtitle}</p>
-                    <Button className="bg-primary-500 text-white px-4 py-2 rounded">{heroHeader.button}</Button>
-                </section>
-            </header>
-        </main>
-        </>
-    )
-}
+  const { heroHeader, mainHeader } = main;
+  const [activeIndex, setActiveIndex] = useState(0);
 
-export default Beranda
+  const handleNavActive = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <>
+      <Navbar />
+      <main className="h-screen w-full flex flex-col px-[120px]">
+        <header
+          className={`flex flex-col items-center w-full h-[400px] bg-[url('${heroHeader.image}')] bg-cover bg-center bg-no-repeat rounded-xl my-16`}
+        >
+          <section
+            className={`flex flex-col items-center justify-center bg-[#000000CC] h-full w-full px-[140px] py-16 gap-6 text-white text-center rounded-xl`}
+          >
+            <h1 className="text-3xl xl:text-5xl font-bold font-pop">
+              {heroHeader.title}
+            </h1>
+            <p>{heroHeader.subtitle}</p>
+            <Button className="bg-primary-500 px-4">{heroHeader.button}</Button>
+          </section>
+        </header>
+        <main className="flex flex-col gap-8">
+          <header className="flex flex-col gap-[10px]">
+            <h2 className="font-pop text-4xl font-semibold">
+              {mainHeader.title}
+            </h2>
+            <p className="text-slate-500 text-base font-medium">
+              {mainHeader.subtitle}
+            </p>
+          </header>
+          <section className="font-medium">
+            {mainHeader.navbar.map((item, index) => (
+              <button
+                key={index}
+                type="button"
+                className={`pr-9 py-3 hover:text-tertiary-500 relative ${
+                  activeIndex === index ? "text-tertiary-500" : "text-slate-500"
+                }`}
+                onClick={() => handleNavActive(index)}
+              >
+                {item}
+                {activeIndex === index && (
+                  <span className="absolute bottom-0 left-0 w-1/3 h-1 bg-tertiary-500"></span>
+                )}
+              </button>
+            ))}
+          </section>
+        </main>
+      </main>
+    </>
+  );
+};
+
+export default Beranda;
