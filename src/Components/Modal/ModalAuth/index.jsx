14bb30ecc40c55ab2a358a863f82aba6313dde93
heyymasud <heyymasud@gmail.com>
2024-07-18 13:47:46 +0700

    finish

--- src/Components/Modal/ModalAuth/index.jsx
+++ src/Components/Modal/ModalAuth/index.jsx
@@ -3,7 +3,7 @@ const ModalAuth = ({ isOpen, onClose, textH, textP }) => {
     if (!isOpen) return null;
   
     return (
-      <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center">
+      <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center z-50">
         <div className="bg-white p-4 rounded shadow-lg">
           <h2 className="text-xl mb-4">{textH}</h2>
           <p className="mb-4">{textP}</p>
