727a4ef371b15b7e0752797ac52715b7948c52f5
heyymasud <heyymasud@gmail.com>
2024-08-09 05:37:56 +0700

    redeploy

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,16 +4,16 @@ import authSlices from "./slices/authSlices";
 import courseSlices from "./slices/courseSlices";
 
 const store = configureStore({
-    reducer: {
-        cart: cartSlices,
-        auth: authSlices,
-        courses: courseSlices
-    }
-})
-console.log("oncreate store" ,store.getState());
+  reducer: {
+    cart: cartSlices,
+    auth: authSlices,
+    courses: courseSlices,
+  },
+});
+console.log("oncreate store", store.getState());
 
 store.subscribe(() => {
-    console.log("onchange store" ,store.getState());
-})
+  console.log("onchange store", store.getState());
+});
 
-export default store
+export default store;
